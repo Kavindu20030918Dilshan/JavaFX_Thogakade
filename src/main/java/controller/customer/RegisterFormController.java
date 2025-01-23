0416291b503660e6d458c30885ae112fb17c141f
Kavindu Dilshan <kavindu4543@gmail.com>
2025-01-23 21:23:45 +0530

    navigation

---
+++ src/main/java/controller/customer/RegisterFormController.java
@@ -0,0 +1,65 @@
+package controller.customer;
+
+import db.DBConnection;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import model.User;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RegisterFormController extends Component {
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private PasswordField txtPasswordConfirm;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnRegisterOnAction(ActionEvent event)throws SQLException {
+        String SQL = "INSERT INTO users (username,email,password) VALUES(?,?,?)";
+
+        if (txtPassword.getText().equals(txtPasswordConfirm.getText())){
+
+            Connection connection = DBConnection.getInstance().getConnection();
+
+            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM users WHERE email = " + "'" + txtEmail.getText() + "' ");
+
+            if (resultSet.next()){
+                new Alert(Alert.AlertType.ERROR,"User already registered").show();
+            }else{
+                User user = new User(
+                        txtUsername.getText(),
+                        txtEmail.getText(),
+                        txtPassword.getText()
+                );
+
+                PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+                preparedStatement.setString(1,user.getUsername());
+                preparedStatement.setString(2,user.getEmail());
+                preparedStatement.setString(3,user.getPassword());
+                preparedStatement.executeUpdate();
+            }
+
+        }else{
+            JOptionPane.showMessageDialog(this,"Password must be same","Warning",JOptionPane.WARNING_MESSAGE);
+
+        }
+
+    }
+
+}
